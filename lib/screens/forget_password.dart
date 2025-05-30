166150bcd131aaf33eaf3a41fe3489b1e30cd9c2
rae <raelene.chaa@gmail.com>
2025-05-30 19:11:51 +0800

    changes

--- lib/screens/forget_password.dart
+++ lib/screens/forget_password.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ForgetScreen extends StatelessWidget {
-<<<<<<< HEAD
-=======
-  String? email;
->>>>>>> 23d2ace798fb0a6f3cafeb03b4a1d08cadce962b
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +22,6 @@ class ForgetScreen extends StatelessWidget {
               ),
             ),
 
-<<<<<<< HEAD
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
@@ -36,49 +31,6 @@ class ForgetScreen extends StatelessWidget {
                 );
               },
               child: const Text('Send Reset Link'),
-=======
-            const SizedBox(height: 30), // Space between title and form
-            // Container box for the form fields
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: const Color(0xFF344E41),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              
-              child: Form(
-                child: Column(
-                  children: [
-                    // Email field
-                    TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      decoration: const InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(color: Colors.white),
-                        border: OutlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please provide an email address.";
-                        } else if (!value.contains('@')) {
-                          return "Please provide a valid email address.";
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        email = value;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    // Submit button
-                    ElevatedButton(
-                      
-                    )
-                  ],
-                ),
-              ),
->>>>>>> 23d2ace798fb0a6f3cafeb03b4a1d08cadce962b
             ),
           ],
         ),
