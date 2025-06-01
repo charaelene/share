738063ae0690eb3731becca4f0d4b1612bd3024c
rae <raelene.chaa@gmail.com>
2025-06-01 11:22:23 +0800

    Co-authored-by: Dylan <yaboywf@users.noreply.github.com> changes to pantry expiring

--- lib/screens/pantry_expiring.dart
+++ lib/screens/pantry_expiring.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class PantryInventory extends StatelessWidget {
-  const PantryInventory({super.key});
+class PantryExpiring extends StatelessWidget {
+  const PantryExpiring({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +14,7 @@ class PantryInventory extends StatelessWidget {
             'Prepped some meals! Remember to make hot before you eat!',
         'image': 'images/leftovers_container.png',
         'consumed': true,
+        'expired': true,
         'quantity': 1,
         'bgColor': const Color.fromARGB(181, 211, 131, 131),
       },
@@ -22,6 +23,7 @@ class PantryInventory extends StatelessWidget {
         'description': '"Couldn\'t finish this..might eat later"',
         'image': 'images/leftover_bluebowl.png',
         'consumed': true,
+        'expired': true,
         'quantity': 1,
         'bgColor': const Color.fromARGB(181, 211, 131, 131),
       },
@@ -65,7 +67,7 @@ class PantryInventory extends StatelessWidget {
         'quantity': 1,
         'bgColor': const Color.fromARGB(122, 75, 194, 59),
       },
-       {
+      {
         'title': 'I bought milo',
         'description': 'Bought for everyone to share!!',
         'image': 'images/milo_resized.png',
@@ -73,15 +75,7 @@ class PantryInventory extends StatelessWidget {
         'quantity': 1,
         'bgColor': const Color.fromARGB(122, 75, 194, 59),
       },
-       {
-        'title': 'I bought milo',
-        'description': 'Bought for everyone to share!!',
-        'image': 'images/milo_resized.png',
-        'consumed': true,
-        'quantity': 1,
-        'bgColor': const Color.fromARGB(122, 75, 194, 59),
-      },
-       {
+      {
         'title': 'I bought milo',
         'description': 'Bought for everyone to share!!',
         'image': 'images/milo_resized.png',
@@ -101,7 +95,12 @@ class PantryInventory extends StatelessWidget {
           Navigator.pop(context); // Navigate back to the previous screen
         },
       ),
-      backgroundColor: Color.fromRGBO(30, 80, 42, 1.0), // Dark green background
+      backgroundColor: Color.fromRGBO(
+        211,
+        212,
+        172,
+        1.0,
+      ), // Dark green background
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
@@ -109,49 +108,19 @@ class PantryInventory extends StatelessWidget {
         title: Row(
           children: [
             const Text(
-              'Our Pantry',
+              'Expiring Soon',
               style: TextStyle(
                 fontSize: 35,
                 fontStyle: FontStyle.italic,
                 fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(211, 212, 172, 1.0),
+                color: Color.fromRGBO(30, 80, 42, 1.0),
               ),
             ),
             SizedBox(width: 10),
             Icon(
-              FontAwesomeIcons.utensils,
-              color: Color.fromRGBO(211, 212, 172, 1.0),
+              FontAwesomeIcons.clock,
+              color: Color.fromRGBO(30, 80, 42, 1.0),
               size: 30,
-            ),
-            SizedBox(width: 95),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/pantry_editor');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFA3B18A),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(
-                    color: Color.fromRGBO(211, 212, 172, 1.0),
-                    width: 2, // Border width
-                  ),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                minimumSize: const Size(100, 50),
-              ),
-              child: const Text(
-                'Add Item',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
             ),
           ],
         ),
@@ -159,6 +128,28 @@ class PantryInventory extends StatelessWidget {
       body: Column(
         children: [
           const SizedBox(height: 10),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(46, 94, 55, 1.0),
+              borderRadius: BorderRadius.circular(40.0),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "2 items are expiring soon",
+                  style: TextStyle(
+                    color: Color.fromRGBO(211, 212, 172, 1.0),
+                    fontSize: 26.0,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             // Expanded to fill the available space
             child: ListView.builder(
@@ -218,9 +209,29 @@ class PantryInventory extends StatelessWidget {
                         ),
                       ),
 
-                      // Consumed label and edit icon
+                      // Consumed and expired label 
                       Column(
                         children: [
+                          if (item['expired'] == true)
+                           Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 13,
+                              vertical: 4,
+                            ),
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(188, 204, 92, 92),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Text(
+                              'Expired',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 255, 254, 253),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 8),
                           Container(
                             padding: const EdgeInsets.symmetric(
                               horizontal: 8,
@@ -239,8 +250,6 @@ class PantryInventory extends StatelessWidget {
                               ),
                             ),
                           ),
-                          const SizedBox(height: 8),
-                          Icon(FontAwesomeIcons.penToSquare, size: 20),
                         ],
                       ),
                     ],
